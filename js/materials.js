05a4abea388663c3db96e724c477a1c37982d24a
Pinkify_ <huelleigor@gmail.com>
2025-04-09 18:55:02 +0200

    zsxf

--- js/materials.js
+++ js/materials.js
@@ -3,37 +3,22 @@ const countObr     = 4;
 const slajdPerObr  = 1;
 const countSlajdy  = Math.ceil(countObr/slajdPerObr);
 
-let nrObr = 1;
-for(let i = 0; i < countSlajdy; i++){
-    let slajd = '<div id="slajd'+i+'">';
-    for(let j = 0; j < slajdPerObr; j++){
-        if(nrObr <= countObr){
-            slajd += '<div class="obrazek"><img src="obrazki/obr'+nrObr+'.jpg" alt="Obrazek '+nrObr+'"></div>';
-            nrObr++;
-        } else {
-            break;
-        };
-    };
-    slajd += '</div>';
-    galeria.innerHTML += slajd;
-};
-
 const strzLewo      = document.querySelector('#przewinLewo');
 const strzPrawo     = document.querySelector('#przewinPrawo');
 const obrazki       = document.querySelector('#obrazki');       // "Pudełko z czterema obrazkami"
 const trzymak       = document.querySelector('#trzymak');       // Trzyma wszystkie obrazki
 
-const height = obrazki.getBoundingClientRect().height + 4;      // + 4 bo jest gap 4px od flexa w #galeria
+const width = obrazki.getBoundingClientRect().width + 4;      // + 4 bo jest gap 4px od flexa w #galeria
 
 let moveTo = 0;
 strzPrawo.addEventListener('click', () => {
-    if(moveTo >= height*(countSlajdy-1)) return;
-    moveTo += height;
+    if(moveTo >= width*(countSlajdy-1)) return;
+    moveTo += width;
     trzymak.style.left = `-${moveTo}px`;
 });
 strzLewo.addEventListener('click', () => {
     if(moveTo <= 0) return;
-    moveTo -= height;
+    moveTo -= width;
     trzymak.style.left = `-${moveTo}px`;
 });
 
